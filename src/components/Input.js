a4d705a3095b99c04605d8b1d56d17bce26b2055
Mateusz Walter <wuoelte@gmail.com>
2021-08-18 20:10:26 +0200

    zadanie 2

---
+++ src/components/Input.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Input( {bgColor, color, borderSize, borderRadius, borderColor }){
+    return(
+
+    );
+}
+
+export default Input;
---
+++ src/components/Textarea.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Textarea( {bgColor, color, borderSize, borderRadius, borderColor }){
+    return(
+
+    );
+}
+
+export default Textarea;
