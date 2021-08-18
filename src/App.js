bb3bff022ca752fda54c90f6872e2a51f3a8230e
Mateusz Walter <wuoelte@gmail.com>
2021-08-18 18:51:09 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './App.css';
+
+import Button from "./components/Button/index"
+
+
+function App() {
+  return (
+    <Button />
+
+  );
+}
+
+export default App;
